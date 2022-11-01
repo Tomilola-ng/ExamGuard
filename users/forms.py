ca765963157e8c3afc2b5456a5ded5f92d5f6173
Oluwafemi Tomilola <93817917+tee-o28@users.noreply.github.com>
2022-11-01 19:34:09 +0100

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.validators import RegexValidator
+
 
 class CustomUserCreationForm(UserCreationForm):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = forms.CharField(validators=[phone_regex], max_length=17)
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
+        fields = UserCreationForm.Meta.fields + ("email","phone")
