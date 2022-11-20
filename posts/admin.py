4fda46c4147116ad8dc1f18815b2486c4f361518
Oluwafemi Tomilola <93817917+tee-o28@users.noreply.github.com>
2022-11-20 12:17:54 +0100

    Delete posts directory

--- posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from posts.models import Post
-
-# Register your models here.
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from posts.views import PostDetailView , post_list
-
-
-urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('tag/<slug:tag_slug>', post_list, name='post_list_by_tag'),
-    path('<int:id>/', PostDetailView, name='post_detail')
-]
